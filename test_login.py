a45d0be736f9018c5bd96dd17b3bd0f9f15dbde3
Ivan Dmitriev <Realdeathstroke@yandex.ru>
2019-04-28 14:21:36 +0300

    удаление лишнего кода в тесте логина

--- test_login.py
+++ test_login.py
@@ -1,7 +1,5 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from application import Application
 
 @pytest.fixture
