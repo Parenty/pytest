88b39353f6c89b4196ecb6a003f8f99d83460de4
Ivan Dmitriev <Realdeathstroke@yandex.ru>
2019-05-09 14:18:18 +0300

    Фикстура перемещена в общий для всех тестов файл conftest

--- test/test_login.py
+++ test/test_login.py
@@ -1,14 +1,3 @@
-import pytest
-from selenium import webdriver
-from fixture.application import Application
-
-@pytest.fixture
-def app(request):
-	fixture = Application()
-	request.addfinalizer(fixture.destroy)
-	return fixture
-
-
 def test_login(app):
 	app.session.login(username='dmitriev@uchi.ru', password='123')
 	app.session.logout()
