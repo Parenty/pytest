e5cfb09ebd91ad4ac0fdd5e1cbfc6ddc1b6f5262
Ivan Dmitriev <Realdeathstroke@yandex.ru>
2019-05-09 15:51:39 +0300

    Отмена изменений предыдущего коммита

--- conftest.py
+++ conftest.py
@@ -5,6 +5,5 @@ from fixture.application import Application
 @pytest.fixture
 def app(request):
 	fixture = Application()
-	fixture.session.login(username='dmitriev@uchi.ru', password='123')
 	request.addfinalizer(fixture.destroy)
 	return fixture
--- test/test_login.py
+++ test/test_login.py
@@ -1,4 +1,5 @@
 def test_login(app):
+	app.session.login(username='dmitriev@uchi.ru', password='123')
 	app.session.logout()
 
 
