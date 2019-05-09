e84635917f4378c23d7921095d0a9e86a9740d8d
Ivan Dmitriev <Realdeathstroke@yandex.ru>
2019-05-09 15:40:33 +0300

    метод логин выделен в фикстуру

--- conftest.py
+++ conftest.py
@@ -5,5 +5,6 @@ from fixture.application import Application
 @pytest.fixture
 def app(request):
 	fixture = Application()
+	fixture.session.login(username='dmitriev@uchi.ru', password='123')
 	request.addfinalizer(fixture.destroy)
 	return fixture
--- test/test_login.py
+++ test/test_login.py
@@ -1,5 +1,4 @@
 def test_login(app):
-	app.session.login(username='dmitriev@uchi.ru', password='123')
 	app.session.logout()
 
 
