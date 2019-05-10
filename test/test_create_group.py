8ddc9a018aa12f9d1df3469bc4fbba0e9c2565c5
Ivan Dmitriev <Realdeathstroke@yandex.ru>
2019-05-10 22:36:33 +0300

    добавлены комментарии

--- test/test_create_group.py
+++ test/test_create_group.py
@@ -2,4 +2,8 @@ from model.group import Group
 
 def test_create_group(app):
 	app.session.login(username='dmitriev@uchi.ru', password='123')
+	# Здесь:
+	# number_class - номер класса,
+	# name_class - название класса (сделал чтобы вводилось вручную)
+	# subject_list - список с предметами, которые нужно добавить (пока что можно добавить только 4 основнных предмета)
 	app.group.create(Group(number_class = '2 класс', name_class = 'тестовый класс', subject_list =['rus', 'eng', 'env']))
