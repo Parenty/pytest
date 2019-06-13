6a9c002dbe76fc4f318ca69a0734f5bde47be626
Parenty <43603737+Parenty@users.noreply.github.com>
2019-06-13 11:01:54 +0300

    Указан путь к файлу конфига

--- conftest.py
+++ conftest.py
@@ -1,18 +1,22 @@
 import pytest
+import json
+import os.path
 from fixture.application import Application
 
 fixture  = None
+target = None
 
 @pytest.fixture
 def app(request):
 	global fixture
+	global target
 	browser = request.config.getoption('--browser')
-	base_url = request.config.getoption('--baseUrl')
-	if fixture is None:
-		fixture = Application(browser=browser, base_url=base_url)
-	else:
-		if not fixture.is_valid():
-			fixture = Application()
+	if target is None:
+		config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption('--target'))
+		with open(config_file) as config_file:
+			target = json.load(config_file)
+	if fixture is None or not fixture.is_valid():
+		fixture = Application(browser=browser, base_url=target['baseUrl'])
 	return fixture
 
 
@@ -28,4 +32,4 @@ def stop(request):
 
 def pytest_addoption(parser):
 	parser.addoption('--browser', action='store', default='chrome')
-	parser.addoption('--baseUrl', action='store', default='https://uchi.ru/')
+	parser.addoption('--target', action='store', default='target.json')
