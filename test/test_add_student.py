bd7fab26b40d5f8c403259557df0da7d47c9d710
Ivan Dmitriev <Realdeathstroke@yandex.ru>
2019-07-28 13:53:25 +0300

    Добавил папку idea  в gitignore

--- test/test_add_student.py
+++ test/test_add_student.py
@@ -6,18 +6,17 @@ import time
 
 
 @pytest.mark.run(order=3)
-@pytest.mark.parametrize('registration', testdata_reg, ids = [repr(x) for x in testdata_reg])
+@pytest.mark.parametrize('registration', testdata_reg, ids=[repr(x) for x in testdata_reg])
 def test_add_student(app, registration):
+    user = 'dmitriev+57@uchi.ru'
+    pswd = '1'
 
-	user='dmitriev+57@uchi.ru'
-	pswd = '1'
+    app.session.ensure_login(username=user, password=pswd)
+    not_registered = app.group.is_not_registered()
+    if not_registered == True:
+        registration.email = user
+        registration.password = pswd
+        app.registration.reg_teacher(registration)
 
-	app.session.ensure_login(username=user, password=pswd)
-	not_registered = app.group.is_not_registered()
-	if not_registered == True:
-		registration.email = user
-		registration.password = pswd
-		app.registration.reg_teacher(registration)
-
-	old_groups = app.group.get_group_list()
-	app.addstudent.add_student()
+    old_groups = app.group.get_group_list()
+    app.addstudent.add_student()
