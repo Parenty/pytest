d05f84feaee4248b5558b5eb7f5eb52aab20ee40
Parenty <43603737+Parenty@users.noreply.github.com>
2019-08-02 16:48:40 +0300

    Правки

--- model/registration.py
+++ model/registration.py
@@ -1,10 +1,9 @@
 class Reg:
 
-	def __init__(self, number_class = None, name_class = None, subject_list = None, class_code = None, email = None, password = None):
-
-		self.number_class = number_class
-		self.name_class = name_class
-		self.subject_list = subject_list
-		self.email = email
-		self.password = password
-		
+    def __init__(self, number_class=None, name_class=None, subject_list=None, class_code=None, email=None,
+                 password=None):
+        self.number_class = number_class
+        self.name_class = name_class
+        self.subject_list = subject_list
+        self.email = email
+        self.password = password
