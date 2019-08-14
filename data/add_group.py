6265f3e977a17d45a7c9cace43f11f2db1617356
Parenty <43603737+Parenty@users.noreply.github.com>
2019-08-14 15:12:30 +0300

    add testdata

--- data/add_group.py
+++ data/add_group.py
@@ -3,7 +3,20 @@ from model.group import Group
 # Создание группы у зареганного учителя
 testdata = [
 
-    Group(number_class='3', name_class='тестовый класс1', subject_list=['rus', 'eng']),
-    # Group(number_class = '5', name_class = 'тестовый класс2', subject_list =['rus', 'eng', 'geo', 'soc']),
-	# Group(number_class = '7', name_class = 'тестовый класс3', subject_list =['rus', 'eng', 'phys', 'soc'])
+    Group(number_class='0', name_class='тестовый класс1', subject_list=['math']),
+    Group(number_class = '1', name_class = 'тестовый класс2', subject_list =['rus', 'eng', 'env', 'math', 'prog']),
+	Group(number_class = '2', name_class = 'тестовый класс3', subject_list =['rus', 'eng', 'env', 'math', 'prog']),
+Group(number_class = '3', name_class = 'тестовый класс4', subject_list =['rus', 'eng', 'env', 'math', 'prog']),
+Group(number_class = '4', name_class = 'тестовый класс5', subject_list =['rus', 'eng', 'env', 'math', 'prog']),
+Group(number_class = '5', name_class = 'тестовый класс6', subject_list =['rus', 'eng', 'bio', 'math', 'prog', 'geo','soc','hist']),
+Group(number_class = '6', name_class = 'тестовый класс7', subject_list =['rus', 'eng', 'bio', 'math', 'prog', 'geo']),
+Group(number_class = '7', name_class = 'тестовый класс8', subject_list =['rus', 'eng', 'phys', 'math', 'geo']),
+Group(number_class = '8', name_class = 'тестовый класс9', subject_list =['rus', 'eng', 'math', 'chem', 'phys']),
+Group(number_class = '9', name_class = 'тестовый класс10', subject_list =['rus', 'eng', 'math']),
+Group(number_class = '10', name_class = 'тестовый класс11', subject_list =['rus', 'eng', 'math']),
+Group(number_class = '11', name_class = 'тестовый класс12', subject_list =['rus', 'eng', 'math']),
+Group(number_class = '1', name_class = 'тестовый класс13', subject_list =['rus', 'eng', 'math']),
+Group(number_class = '1', name_class = 'тестовый класс14', subject_list =['rus', 'eng', 'math']),
+Group(number_class = '1', name_class = 'тестовый класс15', subject_list =['rus', 'eng', 'math'])
+
 ]
