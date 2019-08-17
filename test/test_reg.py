66bf74ff6a8dd526e0a5684b85798f8537965d39
Ivan Dmitriev <Realdeathstroke@yandex.ru>
2019-08-17 14:31:37 +0300

    fix registration test

--- test/test_reg.py
+++ test/test_reg.py
@@ -1,9 +1,12 @@
 
 import pytest
+
+from data.add_student import testdata_add_student
 from data.add_teacher import testdata_reg
 
 @pytest.mark.run(order=1)
+@pytest.mark.parametrize('students', testdata_add_student, ids=[repr(x) for x in testdata_add_student])
 @pytest.mark.parametrize('registration', testdata_reg, ids = [repr(x) for x in testdata_reg])
-def test_reg_teacher(app, registration):
+def test_reg_teacher(app, registration, students):
 	app.open_home_page()
-	app.registration.reg_teacher(registration)
+	app.registration.reg_teacher(registration, students)
